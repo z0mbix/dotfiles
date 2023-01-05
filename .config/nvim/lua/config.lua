4a98a3cf37adafbe46cabcb4ae88b4da8a19ca07
z0mbix <zombie@zombix.org>
2023-01-05 13:22:06 +0000

    neovim: add latest config updates

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -173,7 +173,6 @@ require("telescope").setup({
 })
 
 require("telescope").load_extension("fzf")
-require("telescope").load_extension("urlview")
 
 -- nvim-compe
 require("compe").setup({
@@ -262,54 +261,8 @@ require("nvim-web-devicons").setup({})
 -- FTerm
 require("FTerm").setup({})
 
--- https://github.com/kevinhwang91/nvim-ufo
--- require('ufo').setup()
-
-local handler = function(virtText, lnum, endLnum, width, truncate)
-  local newVirtText = {}
-  local suffix = ("  %d "):format(endLnum - lnum)
-  local sufWidth = vim.fn.strdisplaywidth(suffix)
-  local targetWidth = width - sufWidth
-  local curWidth = 0
-  for _, chunk in ipairs(virtText) do
-    local chunkText = chunk[1]
-    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-    if targetWidth > curWidth + chunkWidth then
-      table.insert(newVirtText, chunk)
-    else
-      chunkText = truncate(chunkText, targetWidth - curWidth)
-      local hlGroup = chunk[2]
-      table.insert(newVirtText, { chunkText, hlGroup })
-      chunkWidth = vim.fn.strdisplaywidth(chunkText)
-      -- str width returned from truncate() may less than 2nd argument, need padding
-      if curWidth + chunkWidth < targetWidth then
-        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-      end
-      break
-    end
-    curWidth = curWidth + chunkWidth
-  end
-  table.insert(newVirtText, { suffix, "MoreMsg" })
-  return newVirtText
-end
-
--- global handler
-require("ufo").setup({
-  fold_virt_text_handler = handler,
-})
-
--- buffer scope handler
--- will override global handler if it is existed
-local bufnr = vim.api.nvim_get_current_buf()
-require("ufo").setFoldVirtTextHandler(bufnr, handler)
-
 -- https://github.com/m-demare/hlargs.nvim
 require("hlargs").setup()
-
--- https://github.com/axieax/urlview.nvim
-require("urlview").setup({
-  default_picker = "telescope",
-})
 
 -- https://github.com/chentoast/marks.nvim
 require("marks").setup()
