b0c1d28a8bf08ac0a77c13f1e6a287cb95882018
z0mbix <zombie@zombix.org>
2023-07-25 22:08:08 +0100

    Add env var to enable copilot

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -323,44 +323,47 @@ null_ls.setup({
 -- require("Bekaboo/dropbar.nvim").setup()
 
 -- https://github.com/zbirenbaum/copilot.lua
-require("copilot").setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>",
+local copilot_enabled = os.getenv("COPILOT_ENABLED")
+if copilot_enabled == "true" then
+  require("copilot").setup({
+    panel = {
+      enabled = true,
+      auto_refresh = false,
+      keymap = {
+        jump_prev = "[[",
+        jump_next = "]]",
+        accept = "<CR>",
+        refresh = "gr",
+        open = "<M-CR>",
+      },
+      layout = {
+        position = "bottom", -- | top | left | right
+        ratio = 0.4,
+      },
     },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4,
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 75,
+      keymap = {
+        accept = "<M-l>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
     },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
+    filetypes = {
+      yaml = true,
+      markdown = true,
+      help = false,
+      ["."] = false,
     },
-  },
-  filetypes = {
-    yaml = true,
-    markdown = true,
-    help = false,
-    ["."] = false,
-  },
-  copilot_node_command = "node", -- Node.js version must be > 16.x
-  server_opts_overrides = {},
-})
+    copilot_node_command = "node", -- Node.js version must be > 16.x
+    server_opts_overrides = {},
+  })
+end
 
 -- https://github.com/filipdutescu/renamer.nvim
 require("renamer").setup()
