8b6c880283920baa3161a427cd0d0ec074d048cd
z0mbix <zombie@zombix.org>
2025-05-10 12:37:28 +0100

    Update Ghostty and zed settings

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     set fish_greeting
 
     fish_add_path ~/bin
+    fish_add_path ~/.local/bin
     fish_add_path ~/.cargo/bin
     fish_add_path ~/.krew/bin
     fish_add_path ~/go/bin
@@ -12,10 +13,3 @@ if status is-interactive
         abbr vi nvim
     end
 end
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-# source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-# Added by Windsurf
-fish_add_path /Users/dwooldridge/.codeium/windsurf/bin
