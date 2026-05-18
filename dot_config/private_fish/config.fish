3707e8721857e4d063ddbc32da57bd29d3bbc41b
z0mbix <zombie@zombix.org>
2026-05-18 16:58:07 +0100

    Clean up fishy things and Brewfile

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,15 +3,9 @@ if status is-interactive
     set fish_color_valid_path
 
     fish_add_path ~/bin
-    fish_add_path ~/.local/bin
+    fish_add_path ~/go/bin
     fish_add_path ~/.cargo/bin
     fish_add_path ~/.krew/bin
-    fish_add_path ~/go/bin
+    fish_add_path ~/.local/bin
     fish_add_path ~/.opencode/bin
-
-    if command --query nvim
-        set -x EDITOR nvim
-        abbr vim nvim
-        abbr vi nvim
-    end
 end
