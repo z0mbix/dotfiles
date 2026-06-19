de44250ca0b9974d3e8b1caef9616d4379f17447
z0mbix <zombie@zombix.org>
2026-06-19 16:13:58 +0100

    Update tmux to be even more awesome. Manage plugins with chezmoi

--- dot_config/private_fish/conf.d/04_binds.fish
+++ dot_config/private_fish/conf.d/04_binds.fish
@@ -2,7 +2,9 @@
 # set -g fish_escape_delay_ms 200
 bind escape,. history-token-search-backward
 bind alt-delete backward-kill-path-component
-bind alt-backspace backward-kill-path-component
+# backward-kill-word stops at punctuation (e.g. '-'), so opt+backspace on
+# "nrtc-dev-ccp" deletes back to the previous '-' rather than the whole token
+bind alt-backspace backward-kill-word
 
 # Right Alt+c to cd into a directory with fzf
 bind ç fzf-cd-widget
